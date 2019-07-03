b0ff188d1bb68d7c0de7ad6117066092d4dc15bc
kongqingyong <kongqingyong@puduedu.com>
2019-07-03 14:23:06 +0800

    Initial commit

---
+++ src/main/java/edu/jiahui/point/controller/DemoController.java
@@ -0,0 +1,34 @@
+package edu.jiahui.point.controller;
+
+import edu.jiahui.point.domain.Demo;
+import edu.jiahui.framework.exceptions.ClientException;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * Copyright (C) 2019 Shanghai JiaHui Edu., Ltd. All rights reserved.
+ * <p>
+ * No parts of this file may be reproduced or transmitted in any form or by any means,
+ * electronic, mechanical, photocopying, recording, or otherwise, without prior written
+ * permission of Shanghai JiaHui Edu., Ltd.
+ *
+ * @author krame
+ * @date 2019/7/2
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/testGet")
+    public String testGet(@RequestParam String testId) {
+
+        throw new ClientException();
+//        return "OK";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/testPost")
+    @ResponseBody
+    public Demo testPost(@RequestBody Demo demo) {
+        return demo;
+    }
+}
