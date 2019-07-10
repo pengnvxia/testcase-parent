f89908c4ef9b16661d659dded395260b8bfcd411
kongqingyong <kongqingyong@puduedu.com>
2019-07-10 11:01:56 +0800

    更新docker中的启动文件

--- dev/start.sh
+++ dev/start.sh
@@ -3,7 +3,7 @@ rm -rf /opt/docker/{{ name }}-0.0.1-SNAPSHOT.jar
 cd /{{ name }}
 mv application* /opt/docker/
 mv {{ name }}-0.0.1-SNAPSHOT.jar /opt/docker
-sleep 10
+sed -i "s#spring.profiles.active=local#spring.profiles.active=dev#g" /opt/docker/application.properties
 time=`date +%F-%H-%M-%S`
 cd /opt/docker/
 docker build -t {{ name }}-dev:$time ./
--- prod/start.sh
+++ prod/start.sh
@@ -3,7 +3,7 @@ rm -rf /opt/docker/{{ name }}-1.0.0-RELEASE.jar
 cd /{{ name }}
 mv application* /opt/docker/
 mv {{ name }}-1.0.0-RELEASE.jar /opt/docker
-sleep 10
+sed -i "s#spring.profiles.active=local#spring.profiles.active=prod#g" /opt/docker/application.properties
 time=`date +%F-%H-%M-%S`
 cd /opt/docker/
 docker build -t {{ name }}-prod:$time ./
