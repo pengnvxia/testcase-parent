61e5e246cb01ee963153474ceb90f9b0cbc2da6c
kongqingyong <kongqingyong@puduedu.com>
2019-09-30 10:56:19 +0800

    更新配置

--- dev/init.sh
+++ dev/init.sh
@@ -1,5 +1,5 @@
 service sshd start
 source /etc/profile
 cd /opt
-java -Xmx1536m -Xms1536m -jar ./{{ name }}-0.0.1-SNAPSHOT.jar &
+java -Xmx1536m -Xms1536m -Xmn1024m -jar ./{{ name }}-0.0.1-SNAPSHOT.jar &
 tail -f /etc/hosts
