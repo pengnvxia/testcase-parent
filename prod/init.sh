ccf96dcd32576d5d9e4dd25d831d6162a44ad4e8
kongqingyong <kongqingyong@puduedu.com>
2019-10-30 15:59:27 +0800

    <refactor>(<location>) : 修改配置项参数 <body> 修改配置项参数 <footer> 关联bugId:无

--- prod/init.sh
+++ prod/init.sh
@@ -1,5 +1,5 @@
 service sshd start
 source /etc/profile
 cd /opt
-java -Xmx3072m -Xms3072m -Xmn2048m -jar ./{{ name }}-1.0.0-RELEASE.jar &
+java -Xmx3072m -Xms3072m -Xmn2048m -jar ./{{ name }}-1.0.0.RELEASE.jar &
 tail -f /etc/hosts
