1a6b721ba0486f259ca214846b4c48fdeb0fe30c
kongqingyong <kongqingyong@puduedu.com>
2019-10-31 15:41:41 +0800

    脚手架更新

--- dev/start.sh
+++ dev/start.sh
@@ -1,13 +1,8 @@
-#删除之前存活的镜像
-docker rm -f `docker ps -a |grep {{ name }}-dev|awk '{print $1}'`
-time=`date +%F-%H-%M-%S`
 #替换文件
 sed -i "s#spring.profiles.active=local#spring.profiles.active=dev#g" /{{ name }}/application.properties
 #进入目录
 cd /{{ name }}
 #构建镜像
-docker build -t {{ name }}-dev:$time ./
-#运行镜像
-docker run -d -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /data/{{ name }}/:/opt/log/ -m 1638M {{ name }}-dev:$time
+docker build -t {{ name }}-dev:$1 ./
 #删除文件
 rm -rf /{{ name }}/*
--- prod/start.sh
+++ prod/start.sh
@@ -1,13 +1,8 @@
-#删除之前存活的镜像
-docker rm -f `docker ps -a |grep {{ name }}-prod|awk '{print $1}'`
-time=`date +%F-%H-%M-%S`
 #替换文件
 sed -i "s#spring.profiles.active=local#spring.profiles.active=prod#g" /{{ name }}/application.properties
 #进入目录
 cd /{{ name }}
 #构建镜像
-docker build -t {{ name }}-prod:$time ./
-#运行镜像
-docker run -d -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /data/{{ name }}/:/opt/log/ -m 3276M {{ name }}-prod:$time
+docker build -t {{ name }}-prod:$1 ./
 #删除文件
 rm -rf /{{ name }}/*
