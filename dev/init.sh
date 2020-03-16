86868f015ca4fa1fe2ffe6393da7b52f0bcd3f1a
krame <kongkrame@gmail.com>
2020-03-16 14:47:44 +0800

    优化模板项目项目结构

--- dev/init.sh
+++ dev/init.sh
@@ -1,5 +1,4 @@
 service sshd start
-source /etc/profile
 cd /opt
 java -Xmx1536m -Xms1536m -Xmn1024m -jar ./{{ name }}-0.0.1-SNAPSHOT.jar &
 tail -f /etc/hosts
--- prod/start.sh
+++
@@ -1,8 +0,0 @@
-#替换文件
-sed -i "s#spring.profiles.active=local#spring.profiles.active=prod#g" /{{ name }}/application.properties
-#进入目录
-cd /{{ name }}
-#构建镜像
-docker build -t {{ name }}-prod:$1 ./
-#删除文件
-rm -rf /{{ name }}/*
