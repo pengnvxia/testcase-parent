b6a3059777688147573d5b152230e7e77621e339
pengyishuang <pengyishuang@puduedu.com>
2021-03-23 14:55:03 +0800

    032303java更新

---
+++ testcase-common/src/main/java/edu/jiahui/testcase/common/constant/ServiceConstant.java
@@ -0,0 +1,18 @@
+package edu.jiahui.testcase.constant;
+
+/**
+ * <p>
+ * Copyright (C) 2019 Shanghai JiaHui Edu., Ltd. All rights reserved.
+ * <p>
+ * No parts of this file may be reproduced or transmitted in any form or by any means,
+ * electronic, mechanical, photocopying, recording, or otherwise, without prior written
+ * permission of Shanghai JiaHui Edu., Ltd.
+ *
+ * @author krame
+ * @date 2019/11/14
+ */
+public class ServiceConstant {
+
+    public static final String SERVICE_NAME = "testcase";
+    public static final String ROOT_URL = "/" + SERVICE_NAME;
+}
